094dacb52eef57e00009c0a4636c4553c653256b
agosmay <agostinamgatti@gmail.com>
2023-01-04 15:55:41 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+import { Counter } from './features/counter/Counter';
+
+const App = () => {
+ 
+
+  return (
+    <div className="App">
+      <h1>Counter with Redux</h1>
+	  <Counter />
+    </div>
+  )
+}
+
+export default App
